b778df42e23729460a11f9337482c76a955f5ee1
RasheedRashid <55117772+RasheedRashid@users.noreply.github.com>
2025-12-30 23:30:11 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "VIMAR",
+  title: "VIRAM",
   description: "Supporting manufacturers to become more competitive",
 };
 
